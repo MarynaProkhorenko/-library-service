7ac7e8ff4886db7369c8ac335485c862281170af
Maryna Prokhorenko <maryna.prokhorenko11@gmail.com>
2023-04-13 11:54:31 +0300

    Added book model

--- book_service/models.py
+++ book_service/models.py
@@ -1,3 +1,29 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    class CoverChoice(models.TextChoices):
+        HARD = "Hard"
+        SOFT = "Soft"
+
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    cover = models.CharField(max_length=4, choices=CoverChoice.choices)
+    inventory = models.ImageField(
+        validators=MinValueValidator(0),
+        default=0,
+    )
+    daily_fee = models.DecimalField(
+        max_digits=5,
+        decimal_places=2,
+        default=0,
+    )
+
+    class Meta:
+        ordering = ["title"]
+
+    def __str__(self) -> str:
+        return f"{self.title} {self.author}"
+
+
