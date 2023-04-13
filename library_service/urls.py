d9edf66db7fb5fd30eaff9e30628b36f7e72939a
Maryna Prokhorenko <maryna.prokhorenko11@gmail.com>
2023-04-13 15:02:34 +0300

    Added path to user/urls.py to project urls

--- library_service/urls.py
+++ library_service/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/books/", include("book_service.urls", namespace="book")),
+    path("api/user/", include("user.urls", namespace="user")),
 ]
